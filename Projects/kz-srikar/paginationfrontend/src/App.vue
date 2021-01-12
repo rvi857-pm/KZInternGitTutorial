e9fda1ba1d6cf6f5c2ccb1274a23c87d80db2349
= <=>
2021-01-12 22:54:18 +0530

    ui improvement

--- Projects/kz-srikar/paginationfrontend/src/App.vue
+++ Projects/kz-srikar/paginationfrontend/src/App.vue
@@ -1,45 +1,88 @@
 <template>
   <div id="app">
+    <div>
+      <b-navbar toggleable="lg" type="dark" variant="info">
+        <b-navbar-brand href="#">PAGINATION AND SEARCH</b-navbar-brand>
+        <div v-if="isPaginated">
+            <b-button size="sm" @click="togglePagination"
+              >Remove Pagination</b-button
+            >
+        </div>
+        <div v-else>
+            <b-button size="sm" @click="togglePagination"
+              >Add Pagination</b-button
+            >
+        </div>
+      </b-navbar>
+    </div>
+
     <div v-if="isPaginated">
-      <b-button @click="togglePagination">Remove Pagination</b-button>
-      <b-container fluid>
+      <b-col class="btn" sm="2">
         <b-pagination
           v-model="currentPage"
           :total-rows="totalResults"
           :per-page="perPage"
           aria-controls="my-table"
         ></b-pagination>
-        <label>Page Size</label>
-        <input v-model="perPage" type="number" :min="1" inline controls/>
-        <label>Account Name</label>
-        <input v-model="accountName" placeholder="Account Name"/>
-        <label>Ip Domain</label>
-        <input v-model="ipDomain" placeholder="Ip Domain"/>
-        <label>Ip Geo City</label>
-        <input v-model="ipGeoCity" placeholder="Ip Geo City"/>
-        <label>Ip Geo State</label>
-        <input v-model="ipGeoState" placeholder="Ip Geo State"/>
-        <label>Ip Geo Country</label>
-        <input v-model="ipGeoCountry" placeholder="Ip Geo Country"/>
-        <label>Type</label>
-        <input v-model="type" placeholder="Type"/>
-        <label>Sfdc Account Id</label>
-        <input v-model="sfdcAccountId" placeholder="Sfdc Account Id"/>
+      </b-col>
+      <b-container fluid>
+        <b-row>
+          <b-col sm="3" class="pb-2">
+            <label class="lab">Page Size</label><br/>
+            <input class="in" v-model="perPage" type="number" :min="1" inline controls />
+          </b-col>
+          <b-col sm="3" class="pb-2">
+            <label class="lab">Account Name</label><br/>
+            <input class="in" v-model="accountName" placeholder="Account Name" />
+          </b-col>
+          <b-col sm="3" class="pb-2">
+            <label class="lab">Ip Domain</label><br/>
+            <input class="in" v-model="ipDomain" placeholder="Ip Domain" />
+          </b-col>
+          <b-col sm="3" class="pb-2">
+            <label class="lab">Ip Geo City</label><br/>
+            <input class="in" v-model="ipGeoCity" placeholder="Ip Geo City" />
+          </b-col>
+        </b-row>
+        <b-row>
+          <b-col sm="3" class="pb-2">
+            <label class="lab">Ip Geo State</label><br/>
+            <input class="in" v-model="ipGeoState" placeholder="Ip Geo State" />
+          </b-col>
+          <b-col sm="3" class="pb-2">
+            <label class="lab">Ip Geo Country</label><br/>
+            <input class="in" v-model="ipGeoCountry" placeholder="Ip Geo Country" />
+          </b-col>
+          <b-col sm="3" class="pb-2">
+            <label class="lab">Type</label><br/>
+            <input class="in" v-model="type" placeholder="Type" />
+          </b-col>
+          <b-col sm="3" class="pb-2">
+            <label class="lab">Sfdc Account Id</label><br/>
+            <input class="in" v-model="sfdcAccountId" placeholder="Sfdc Account Id" />
+          </b-col>
+        </b-row>
+        <br />
       </b-container>
-      <Page :pageSize="getPageSize()" 
-      :currentPage="currentPage" 
-      :accountName="accountName"
-      :ipDomain="ipDomain"
-      :ipGeoCity="ipGeoCity"
-      :ipGeoState="ipGeoState"
-      :ipGeoCountry="ipGeoCountry"
-      :type="type"
-      :sfdcAccountId="sfdcAccountId"
-       />
+      <Page
+        :pageSize="getPageSize()"
+        :currentPage="currentPage"
+        :accountName="accountName"
+        :ipDomain="ipDomain"
+        :ipGeoCity="ipGeoCity"
+        :ipGeoState="ipGeoState"
+        :ipGeoCountry="ipGeoCountry"
+        :type="type"
+        :sfdcAccountId="sfdcAccountId"
+      />
     </div>
     <div v-else>
-      <b-button @click="togglePagination">Add Pagination</b-button>
-      <b-table id="my-table" :items="results" :per-page="totalResults" small></b-table>
+      <b-table
+        id="my-table"
+        :items="results"
+        :per-page="totalResults"
+        small
+      ></b-table>
     </div>
   </div>
 </template>
@@ -65,7 +108,7 @@ export default {
       ipGeoState: "",
       ipGeoCountry: "",
       type: "",
-      sfdcAccountId: ""
+      sfdcAccountId: "",
     };
   },
   methods: {
@@ -73,11 +116,39 @@ export default {
       this.isPaginated = !this.isPaginated;
     },
     getPageSize() {
-      if(this.perPage == "")
-        return 0;
+      if (this.perPage == "") return 0;
       return parseInt(this.perPage);
+    },
+    onChangeInput() {
+      this.currentPage = 1;
     }
   },
+  watch: {
+        perPage: function() {
+            this.onChangeInput();
+        },
+        accountName: function() {
+            this.onChangeInput();
+        },
+        ipDomain: function() {
+            this.onChangeInput();
+        },
+        ipGeoCity: function() {
+            this.onChangeInput();
+        },
+        ipGeoState: function() {
+            this.onChangeInput();
+        },
+        ipGeoCountry: function() {
+            this.onChangeInput();
+        },
+        type: function() {
+            this.onChangeInput();
+        },
+        sfdcAccountId: function() {
+            this.onChangeInput();
+        },
+    },
   mounted: function () {
     fetch("http://localhost:8080/accounts/", {
       method: "get",
@@ -101,6 +172,14 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+.btn {
+  padding: 20px;
+  margin: 20px;
+}
+.lab {
+  text-align: center;
+  margin-top: 10px;
+  color: rgb(190, 58, 102);
 }
 </style>
