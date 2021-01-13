52a61984fe703fbdde13e56c327f5ae9ee5a95cf
= <=>
2021-01-13 12:37:59 +0530

    Add bootstrap to the project

--- Projects/kz-shiva/accounts/src/main.js
+++ Projects/kz-shiva/accounts/src/main.js
@@ -1,4 +1,13 @@
 import { createApp } from 'vue'
+
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+Vue.use(BootstrapVue)
+Vue.use(IconsPlugin)
+
 import App from './App.vue'
 
 createApp(App).mount('#app')
