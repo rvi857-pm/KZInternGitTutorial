d952a11290aa933f285e9381d928e4cc2c766558
= <=>
2021-01-13 14:36:16 +0530

    Implement the Home component

---
+++ Projects/kz-shiva/accounts/src/components/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="header">
+    <h1>
+      Welcome to Kwanzoo Accounts!!
+    </h1>
+  </div>
+</template>
+
+<script>
+
+export default {
+  
+}
+</script>
+
+<style>
+#header {
+  margin-top: 120px;
+}
+</style>
