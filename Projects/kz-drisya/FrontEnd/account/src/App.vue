dd077109079038c1281d66e1280ac2433cb584f1
Drisya POnmari <111701031@smail.iitpkd.ac.in>
2021-01-13 15:58:20 +0530

    successfully fetched data from the api

--- Projects/kz-drisya/FrontEnd/account/src/App.vue
+++ Projects/kz-drisya/FrontEnd/account/src/App.vue
@@ -1,19 +1,23 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    {{ msg }}
+    <Account />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Account from "./components/Acounts";
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    Account,
+  },
+  data() {
+    return {
+      msg: "hey",
+    };
+  },
+};
 </script>
 
 <style>
---
+++ Projects/kz-drisya/FrontEnd/account/src/components/Acounts.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <ul>
+      <li class="list-group-item" v-for="friend in friends" v-bind:key="friend">
+        {{ friend.id }}
+      </li>
+    </ul>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      friends: [],
+    };
+  },
+  mounted: function () {
+    console.log("mounted");
+    fetch(`http://localhost:8080/accounts?page=1`, {
+      method: "get",
+    })
+      .then((response) => {
+        return response.json();
+      })
+      .then((jsonResponse) => {
+        this.friends = jsonResponse;
+      });
+    // console.log(this.friends);
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
