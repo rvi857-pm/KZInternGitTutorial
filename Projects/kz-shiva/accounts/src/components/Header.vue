c1c469ed3cb28b3fb9c739bae1e31a272bca4474
= <=>
2021-01-13 14:17:51 +0530

    Basic template of the UI

---
+++ Projects/kz-shiva/accounts/src/components/Header.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <b-nav tabs>
+      <b-navbar-brand>Accounts</b-navbar-brand>
+      <b-nav-item>Home</b-nav-item>
+      <b-nav-item>Search</b-nav-item>
+    </b-nav>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Header"
+}
+</script>
+
+<style>
+
+</style>
