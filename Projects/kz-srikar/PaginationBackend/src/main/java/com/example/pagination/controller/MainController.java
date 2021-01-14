b451423d60ee56a8f5d0d61ed31e5a8d4ae60f58
= <=>
2021-01-14 20:05:41 +0530

    used example matcher for universal search

--- Projects/kz-srikar/PaginationBackend/src/main/java/com/example/pagination/controller/MainController.java
+++ Projects/kz-srikar/PaginationBackend/src/main/java/com/example/pagination/controller/MainController.java
@@ -28,8 +28,18 @@ public class MainController {
 		int page_size = reqParam.containsKey("page_size") ? Integer.parseInt(reqParam.get("page_size")) : 10;
 
 		if (reqParam.containsKey("name")) {
-			String name = "%" + reqParam.get("name") + "%";
-			return accountRepository.findBySingleSearch(name, (page - 1) * page_size, page_size);
+			String name = reqParam.get("name");
+			Account account = new Account();
+			account.setAccount_name(name);
+			account.setIp_domain(name);
+			account.setIp_geo_city(name);
+			account.setIp_geo_state(name);
+			account.setIp_geo_country(name);
+			account.setType(name);
+			account.setSfdc_account_id(name);
+			Pageable pageableInstance = PageRequest.of(page - 1, page_size);
+			Example<Account> universalSearchExample = Example.of(account, generateUniversalSearchMatcher());
+			return accountRepository.findAll(universalSearchExample, pageableInstance);
 		}
 
 		Account account = new Account();
@@ -41,10 +51,10 @@ public class MainController {
 		account.setIp_geo_country(reqParam.get("ip_geo_country"));
 		account.setType(reqParam.get("type"));
 		account.setSfdc_account_id(reqParam.get("sfdc_account_id"));
-		
-		Pageable pageableInstance = PageRequest.of(page-1, page_size);
+
+		Pageable pageableInstance = PageRequest.of(page - 1, page_size);
 		Example<Account> compoundSearchExample = Example.of(account, generateCompoundSearchMatcher());
-		return accountRepository.findAll(compoundSearchExample,pageableInstance);
+		return accountRepository.findAll(compoundSearchExample, pageableInstance);
 	}
 	
 	private ExampleMatcher generateCompoundSearchMatcher() {
@@ -57,6 +67,18 @@ public class MainController {
 				.withMatcher("type", ExampleMatcher.GenericPropertyMatchers.contains())
 				.withMatcher("sfdc_account_id", ExampleMatcher.GenericPropertyMatchers.contains());
 		return compoundSearchMatcher;
+	}
+
+	private ExampleMatcher generateUniversalSearchMatcher() {
+		ExampleMatcher universalSearchMatcher = ExampleMatcher.matchingAny()
+				.withMatcher("account_name", ExampleMatcher.GenericPropertyMatchers.contains())
+				.withMatcher("ip_domain", ExampleMatcher.GenericPropertyMatchers.contains())
+				.withMatcher("ip_geo_city", ExampleMatcher.GenericPropertyMatchers.contains())
+				.withMatcher("ip_geo_state", ExampleMatcher.GenericPropertyMatchers.contains())
+				.withMatcher("ip_geo_country", ExampleMatcher.GenericPropertyMatchers.contains())
+				.withMatcher("type", ExampleMatcher.GenericPropertyMatchers.contains())
+				.withMatcher("sfdc_account_id", ExampleMatcher.GenericPropertyMatchers.contains());
+		return universalSearchMatcher;
 	}
 
 }
--- Projects/kz-srikar/PaginationBackend/src/main/java/com/example/pagination/dao/AccountRepository.java
+++ Projects/kz-srikar/PaginationBackend/src/main/java/com/example/pagination/dao/AccountRepository.java
@@ -1,21 +1,8 @@
 package com.example.pagination.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
-
 import com.example.pagination.model.Account;
 
-import java.util.List;
-
 public interface AccountRepository  extends JpaRepository<Account, Integer> {
-	@Query(value="select * from account where "
-			+ "account_name like :#{#name} or "
-			+ "ip_domain like :#{#name} or "
-			+ "ip_geo_city like :#{#name} or "
-			+ "ip_geo_state like :#{#name} or "
-			+ "ip_geo_country like :#{#name} or "
-			+ "type like :#{#name} or "
-			+ "sfdc_account_id like :#{#name} limit :#{#offset}, :#{#limit}", nativeQuery=true)
-	public List<Account> findBySingleSearch(@Param("name") String name, @Param("offset") int offset, @Param("limit") int limit);
+
 }
