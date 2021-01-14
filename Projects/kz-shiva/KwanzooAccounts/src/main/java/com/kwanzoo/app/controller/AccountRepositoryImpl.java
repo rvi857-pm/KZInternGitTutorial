bc43dd918abdcea68bbe70b38403c3ae23e229fe
= <=>
2021-01-14 18:56:56 +0530

    Format backend code

--- Projects/kz-shiva/KwanzooAccounts/src/main/java/com/kwanzoo/app/controller/AccountRepositoryImpl.java
+++ Projects/kz-shiva/KwanzooAccounts/src/main/java/com/kwanzoo/app/controller/AccountRepositoryImpl.java
@@ -23,8 +23,8 @@ public class AccountRepositoryImpl {
 	@Autowired
 	private AccountRepository accRepo;
 
-	public Account probe( Map<String, String> filter ) {
-		
+	public Account probe(Map<String, String> filter) {
+
 		// Creates an probe object based on the filter1.
 		Account account = new Account();
 
@@ -35,11 +35,11 @@ public class AccountRepositoryImpl {
 		account.setIpGeoCountry(filter.get("country"));
 		account.setType(filter.get("type"));
 		account.setSfdcAccountId(filter.get("id"));
-		
+
 		return account;
-		
+
 	}
-	
+
 	public ExampleMatcher rules() {
 
 		ExampleMatcher expMatcher = ExampleMatcher.matchingAll()
@@ -50,13 +50,13 @@ public class AccountRepositoryImpl {
 				.withMatcher("ipGeoCountry", ExampleMatcher.GenericPropertyMatchers.contains())
 				.withMatcher("type", ExampleMatcher.GenericPropertyMatchers.contains())
 				.withMatcher("sfdcAccountId", ExampleMatcher.GenericPropertyMatchers.contains());
-		
+
 		return expMatcher;
-		
+
 	}
 
 	@GetMapping(path = "/accounts")
-	public List<Account> getAccountByParams( @RequestParam Map<String, String> filter ) {		
+	public List<Account> getAccountByParams(@RequestParam Map<String, String> filter) {
 
 		Example<Account> accountExample = Example.of(probe(filter), rules());
 		return accRepo.findAll(accountExample);
@@ -65,21 +65,21 @@ public class AccountRepositoryImpl {
 
 	@GetMapping(path = "/accounts", params = "page")
 	public Page<Account> getAccountsByPage(@RequestParam int page, @RequestParam Map<String, String> filter) {
-		
+
 		Pageable obj = PageRequest.of(page, 10);
 		Example<Account> accountExample = Example.of(probe(filter), rules());
 		return accRepo.findAll(accountExample, obj);
 
 	}
-	
-	@GetMapping(path = "/accounts", params = {"page", "page_size"})
-	public Page<Account> getAccountsByPage(@RequestParam int page, @RequestParam("page_size") int size, @RequestParam Map<String, String> filter) {
-		
+
+	@GetMapping(path = "/accounts", params = { "page", "page_size" })
+	public Page<Account> getAccountsByPage(@RequestParam int page, @RequestParam("page_size") int size,
+			@RequestParam Map<String, String> filter) {
+
 		Pageable obj = PageRequest.of(page, size);
 		Example<Account> accountExample = Example.of(probe(filter), rules());
 		return accRepo.findAll(accountExample, obj);
 
 	}
-
 
 }
