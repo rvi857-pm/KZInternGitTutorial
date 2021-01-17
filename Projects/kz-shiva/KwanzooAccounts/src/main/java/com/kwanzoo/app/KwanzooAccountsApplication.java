9aa9983b644c9bd05b135cc3e32c68178418a6f5
= <=>
2021-01-17 16:24:29 +0530

    Add Universal search without filtering

--- Projects/kz-shiva/KwanzooAccounts/src/main/java/com/kwanzoo/app/KwanzooAccountsApplication.java
+++ Projects/kz-shiva/KwanzooAccounts/src/main/java/com/kwanzoo/app/KwanzooAccountsApplication.java
@@ -2,11 +2,13 @@ package com.kwanzoo.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
+@EnableCaching
 public class KwanzooAccountsApplication {
 
 	public static void main(String[] args) {
--- Projects/kz-shiva/KwanzooAccounts/src/main/java/com/kwanzoo/app/controller/AccountRepositoryImpl.java
+++ Projects/kz-shiva/KwanzooAccounts/src/main/java/com/kwanzoo/app/controller/AccountRepositoryImpl.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Example;
 import org.springframework.data.domain.ExampleMatcher;
 import org.springframework.data.domain.Page;
@@ -25,31 +26,45 @@ public class AccountRepositoryImpl {
 
 	public Account probe(Map<String, String> filter) {
 
-		// Creates an probe object based on the filter1.
 		Account account = new Account();
-
-		account.setName(filter.get("name"));
-		account.setIpDomain(filter.get("domain"));
-		account.setCity(filter.get("city"));
-		account.setState(filter.get("state"));
-		account.setCountry(filter.get("country"));
-		account.setType(filter.get("type"));
-		account.setSalesforceId(filter.get("id"));
-
+		if (filter.get("search") != null) {
+			account.setName(filter.get("search"));
+			account.setIpDomain(filter.get("search"));
+			account.setCity(filter.get("search"));
+			account.setState(filter.get("search"));
+			account.setCountry(filter.get("search"));
+			account.setType(filter.get("search"));
+			account.setSalesforceId(filter.get("search"));
+		} else {
+			account.setName(filter.get("name"));
+			account.setIpDomain(filter.get("ipDomain"));
+			account.setCity(filter.get("city"));
+			account.setState(filter.get("state"));
+			account.setCountry(filter.get("country"));
+			account.setType(filter.get("type"));
+			account.setSalesforceId(filter.get("id"));
+		}
 		return account;
 
 	}
 
-	public ExampleMatcher rules() {
+	public ExampleMatcher rules(Map<String, String> filter) {
 
-		ExampleMatcher expMatcher = ExampleMatcher.matchingAll()
-				.withMatcher("name", ExampleMatcher.GenericPropertyMatchers.contains())
-				.withMatcher("ipDomain", ExampleMatcher.GenericPropertyMatchers.contains())
-				.withMatcher("city", ExampleMatcher.GenericPropertyMatchers.contains())
-				.withMatcher("state", ExampleMatcher.GenericPropertyMatchers.contains())
-				.withMatcher("country", ExampleMatcher.GenericPropertyMatchers.contains())
-				.withMatcher("type", ExampleMatcher.GenericPropertyMatchers.contains())
-				.withMatcher("salesforceId", ExampleMatcher.GenericPropertyMatchers.contains());
+		ExampleMatcher expMatcher = filter.get("search") != null
+				? ExampleMatcher.matchingAny().withMatcher("name", ExampleMatcher.GenericPropertyMatchers.contains())
+						.withMatcher("ipDomain", ExampleMatcher.GenericPropertyMatchers.contains())
+						.withMatcher("city", ExampleMatcher.GenericPropertyMatchers.contains())
+						.withMatcher("state", ExampleMatcher.GenericPropertyMatchers.contains())
+						.withMatcher("country", ExampleMatcher.GenericPropertyMatchers.contains())
+						.withMatcher("type", ExampleMatcher.GenericPropertyMatchers.contains())
+						.withMatcher("salesforceId", ExampleMatcher.GenericPropertyMatchers.contains())
+				: ExampleMatcher.matchingAll().withMatcher("name", ExampleMatcher.GenericPropertyMatchers.contains())
+						.withMatcher("ipDomain", ExampleMatcher.GenericPropertyMatchers.contains())
+						.withMatcher("city", ExampleMatcher.GenericPropertyMatchers.contains())
+						.withMatcher("state", ExampleMatcher.GenericPropertyMatchers.contains())
+						.withMatcher("country", ExampleMatcher.GenericPropertyMatchers.contains())
+						.withMatcher("type", ExampleMatcher.GenericPropertyMatchers.contains())
+						.withMatcher("salesforceId", ExampleMatcher.GenericPropertyMatchers.contains());
 
 		return expMatcher;
 
@@ -58,7 +73,7 @@ public class AccountRepositoryImpl {
 	@GetMapping(path = "/accounts")
 	public List<Account> getAccountByParams(@RequestParam Map<String, String> filter) {
 
-		Example<Account> accountExample = Example.of(probe(filter), rules());
+		Example<Account> accountExample = Example.of(probe(filter), rules(filter));
 		return accRepo.findAll(accountExample);
 
 	}
@@ -67,7 +82,7 @@ public class AccountRepositoryImpl {
 	public Page<Account> getAccountsByPage(@RequestParam int page, @RequestParam Map<String, String> filter) {
 
 		Pageable obj = PageRequest.of(page, 10);
-		Example<Account> accountExample = Example.of(probe(filter), rules());
+		Example<Account> accountExample = Example.of(probe(filter), rules(filter));
 		return accRepo.findAll(accountExample, obj);
 
 	}
@@ -77,7 +92,7 @@ public class AccountRepositoryImpl {
 			@RequestParam Map<String, String> filter) {
 
 		Pageable obj = PageRequest.of(page, size);
-		Example<Account> accountExample = Example.of(probe(filter), rules());
+		Example<Account> accountExample = Example.of(probe(filter), rules(filter));
 		return accRepo.findAll(accountExample, obj);
 
 	}
