eec67d2a4dbb8522de676c0bfb3b352a99b5fdb4
= <=>
2021-01-18 17:55:35 +0530

    Change the header and remove unneccessary files

--- Projects/kz-shiva/accounts/src/components/Header.vue
+++ Projects/kz-shiva/accounts/src/components/Header.vue
@@ -1,15 +1,7 @@
 <template>
 	<div>
 		<b-navbar toggleable="lg" type="dark" variant="dark">
-			<b-navbar-brand href="#">Kwanzoo</b-navbar-brand>
-
-			<b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
-
-			<b-collapse id="nav-collapse" is-nav>
-				<b-navbar-nav>
-					<b-nav-item active>Accounts</b-nav-item>
-				</b-navbar-nav>
-			</b-collapse>
+			<b-navbar-brand href="#">kz-shiva</b-navbar-brand>
 		</b-navbar>
 	</div>
 </template>
