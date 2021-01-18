0980b6d7b8ee32335555050b55e4d2512c15f214
Drisya POnmari <111701031@smail.iitpkd.ac.in>
2021-01-18 23:30:09 +0530

    methods added to support universal search

--- Projects/kz-drisya/FrontEnd/account/src/components/Acounts.vue
+++ Projects/kz-drisya/FrontEnd/account/src/components/Acounts.vue
@@ -1,31 +1,36 @@
 <template>
-  <div>
-    <label> search here</label>
-    <b-form-input
-      id="input"
-      placeholder=" search here"
-      v-model="input.searchVal"
-    ></b-form-input>
-
+  <div id="app">
+    <div>
+      <label id="labe"> Search</label>
+      <b-form-input
+        id="input"
+        placeholder=" search"
+        v-model="searchVal"
+      ></b-form-input>
+      <br />
+      <label id="labe">Rows per page </label>
+      <b-form-input id="input" v-model="page_size"></b-form-input>
+      <br />
+    </div>
     <div class="overflow-auto">
+      <Table v-bind:page_size="page_size" v-bind:accounts="accounts" />
       <b-pagination
-        v-model="input.page"
-        :per-page="input.page_size"
+        v-model="page"
+        :per-page="page_size"
         :total-rows="1400"
         align="center"
       ></b-pagination>
-      <b-table
-        hover
-        small
-        :items="accounts"
-        :per-page="input.page_size"
-        responsive
-      ></b-table>
     </div>
   </div>
 </template>
+
+
 <script>
+import Table from "@/components/Table";
 export default {
+  components: {
+    Table,
+  },
   data() {
     return {
       accounts: [],
@@ -37,76 +42,103 @@ export default {
         country: "",
         type: "",
         sfdc: "",
-        page: "1",
-        page_size: "10",
-        q: "",
-        searchVal: "",
       },
+      searchVal: "",
+      url: "",
+      page: "1",
+      page_size: "10",
+      q: "",
     };
   },
+
   watch: {
-    input: {
+    searchVal: {
       handler() {
         console.log("watching");
-        this.splitVal(this.input.searchVal);
-        let url = this.getParameter(this.input);
-        console.log(url);
-        console.log(this.input);
-        this.getAccounts(url);
+        this.splitVal();
+        this.getParameter();
+        this.getAccounts();
+      },
+      deep: true,
+      immediate: true,
+    },
+    page: {
+      handler() {
+        this.getParameter();
+        this.getAccounts();
+      },
+      depp: true,
+      immediate: true,
+    },
+    page_size: {
+      handler() {
+        this.getParameter();
+        this.getAccounts();
       },
       deep: true,
       immediate: true,
     },
   },
   mounted() {
-    let url = this.getParameter(this.input);
-    // console.log(url);
-    this.getAccounts(url);
-    console.log(this.accounts);
+    console.log("mounted");
+    this.getParameter();
+    this.getAccounts();
   },
   methods: {
-    splitVal(val) {
+    getKeyValue(pair) {
+      if (!pair.includes(":")) this.q = pair.trim();
+      else {
+        let pair_split = pair.split(":");
+        let key = pair_split[0];
+        this.input[key] = pair_split[1];
+      }
+    },
+    splitVal() {
+      console.log("splitting");
       this.initial();
-      if (!val.includes("?")) {
-        if (!val.includes(":")) this.input.q = val;
-        else {
-          let pair = val.split(":");
-          let key = pair[0];
-          this.input[key] = pair[1];
-        }
+      let val = this.searchVal;
+      let pairs;
+      if (!val.includes(",")) {
+        this.getKeyValue(val);
       } else {
-        console.log("hey");
-        let x = val.split(",");
-        for (let i in x) {
-          if (!x[i].includes(":")) this.input.q = x[i];
-          else {
-            let pair = x[i].split(":");
-            let key = pair[0];
-            this.input[key] = pair[1];
-          }
+        console.log("comma");
+        pairs = val.split(",");
+        for (let i in pairs) {
+          this.getKeyValue(pairs[i], i);
         }
       }
     },
-    initial() {
-      (this.input.name = ""), (this.input.city = ""), (this.input.q = "");
-    },
-    getParameter(account) {
-      const url = `http://localhost:8080/accounts`;
-      let requestParam =
-        url + `?page=${account.page}&page_size=${account.page_size}`;
-      if (account.name !== "") requestParam += `&name=${account.name}`;
-      if (account.city !== "") requestParam += `&city=${account.city}`;
-      if (account.state !== "") requestParam += `&state=${account.state}`;
-      if (account.country !== "") requestParam += `&country=${account.country}`;
-      if (account.type !== "") requestParam += `&type=${account.type}`;
-      if (account.sfdc !== "") requestParam += `&sfdc=${account.sfdc}`;
-      if (account.q !== "") requestParam += `&q=${account.q}`;
-      if (account.domain !== "") requestParam += `&domain=${account.domain}`;
 
-      return requestParam;
+    initial() {
+      (this.input.name = ""),
+        (this.input.city = ""),
+        (this.q = ""),
+        (this.input.type = "");
+      this.input.sfdc = "";
+      this.input.domain = "";
+      this.input.country = "";
+      this.input.state = "";
+      this.page = "1";
     },
-    getAccounts(url) {
-      fetch(url, {
+    getParameter() {
+      const url = `http://localhost:8080/accounts`;
+      let requestParam = url + `?page=${this.page}&page_size=${this.page_size}`;
+      if (this.input.name !== "") requestParam += `&name=${this.input.name}`;
+      if (this.input.city !== "") requestParam += `&city=${this.input.city}`;
+      if (this.input.state !== "") requestParam += `&state=${this.input.state}`;
+      if (this.input.country !== "")
+        requestParam += `&country=${this.input.country}`;
+      if (this.input.type !== "") requestParam += `&type=${this.input.type}`;
+      if (this.input.sfdc !== "") requestParam += `&sfdc=${this.input.sfdc}`;
+      if (this.q !== "") requestParam += `&q=${this.q}`;
+      if (this.input.domain !== "")
+        requestParam += `&domain=${this.input.domain}`;
+
+      this.url = requestParam;
+    },
+    getAccounts() {
+      console.log(this.url);
+      fetch(this.url, {
         method: "get",
       })
         .then((response) => {
@@ -116,7 +148,12 @@ export default {
           return jsonResponse;
         })
         .then((jsonResponse) => {
-          this.accounts = jsonResponse.content;
+          let x = jsonResponse.content;
+          for (let i in x) {
+            delete x[i]["id"];
+            delete x[i]["empty"];
+          }
+          this.accounts = x;
         });
     },
   },
@@ -125,14 +162,18 @@ export default {
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  color: #2c3e50;
   margin-top: 60px;
+  margin-left: 10px;
 }
 #input {
-  width: 20%;
+  width: 10%;
   height: 10%;
+  border-left: 1px;
+  margin-block: 5px;
+}
+#labe {
+  font-size: 20px;
+  font-weight: bold;
+  color: black;
 }
 </style>
