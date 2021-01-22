872c1f047d44fee3916e9e310886d2835ad9736f
= <=>
2021-01-22 22:22:13 +0530

    creates a new account in account table

--- Projects/kz-shiva/databaseScripts/createNewAccount.sql
+++ Projects/kz-shiva/databaseScripts/createNewAccount.sql
@@ -1,25 +1,15 @@
 DELIMITER || 
-drop function if exists createNewAccount;
-CREATE FUNCTION createNewAccount (buyerId varchar(100)) 
-	RETURNS varchar(100) 
-	DETERMINISTIC 
+drop procedure if exists createNewAccount;
+CREATE PROCEDURE createNewAccount (
+		IN buyerId varchar(100),
+		OUT value varchar(100)
+	)
 	BEGIN
-		DECLARE id varchar(100),
-		DECLARE account_name varchar(100),
-		DECLARE ip_domain varchar(100),
-		DECLARE city varchar(100),
-		DECLARE state varchar(100),
-		DECLARE country varchar(100),
-		DECLARE type varchar(100),
-		DECLARE salesforce_id varchar(100)
 
-		set id = 
+		call getVariables(buyerId, @name, @ip_domain, @city, @state, @country, @externalId, @creative);
+		set @id = bin_to_uuid( unhex( md5( concat_ws(' ', @name, @ip_domain, @city, @state, @country, @type, @externalId))));
+		insert into account values( @id, @name, @ip_domain, @city, @state, @country, @type, @externalId );
+		set value = @id;
 
-		insert into account( id, account_name, ip_domain, city, state, country, type, salesforce_id ) 
-		values(
-
-		)
-
-	RETURN value;
-END || 
+	END || 
 DELIMITER ;
