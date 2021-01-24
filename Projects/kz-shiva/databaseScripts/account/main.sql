ef8ab49ea1b5dc918899fe0065ad9d01087885dc
= <=>
2021-01-24 11:58:56 +0530

    file to run account script

--- Projects/kz-shiva/databaseScripts/account/main.sql
+++ Projects/kz-shiva/databaseScripts/account/main.sql
@@ -1,4 +1,5 @@
 -- creates table account
+drop table if exists account;
 create table account ( name varchar(100), ip_domain varchar(100), city varchar(100), state varchar(100), country varchar(100), type varchar(100), salesforce_id varchar(100), one varchar(100), two varchar(100));
 
 -- load data into the table account
@@ -13,4 +14,6 @@ delete from account where name = "";
 -- hash value as the primary id of table
 update account set id = bin_to_uuid( unhex( md5( concat_ws(' ', name, ip_domain, city, state, country, type, salesforce_id))));
 alter table account modify column id varchar(100) FIRST;
+
+-- sets id as the primary key
 alter table account add PRIMARY KEY(id);
