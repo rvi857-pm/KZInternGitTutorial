fc2f8db24b3fef5f17d9ff82ace76393f08a4136
= <=>
2021-01-24 11:58:03 +0530

    to store extra variables together

--- Projects/kz-shiva/databaseScripts/buyer/updateVariables.sql
+++ Projects/kz-shiva/databaseScripts/buyer/updateVariables.sql
@@ -1,9 +1,13 @@
 DELIMITER || 
-drop procedure if exists updateGeo;
-CREATE PROCEDURE updateGeo (
+drop procedure if exists updateVariables;
+CREATE PROCEDURE updateVariables (
 		IN buyerId varchar(100)
 	)
 	BEGIN
+
+		DECLARE sourceVal varchar(100);
+		DECLARE jobLevelVal varchar(100);
+		DECLARE jobFuncVal varchar(100);
 
 		drop temporary table if exists variablesProto;
 		create temporary table variablesProto as select * from temp where temp.opencx_buyer_id = buyerId limit 1;
@@ -12,7 +16,7 @@ CREATE PROCEDURE updateGeo (
 		set jobLevelVal = (select job_level from variablesProto);
 		set jobFuncVal = (select job_function from variablesProto);
 
-        insert into variables (buyerId, sourceVal, jobLevelVal, jobFuncVal);
+        insert into variables values(buyerId, sourceVal, jobLevelVal, jobFuncVal);
 
 	END ||
 DELIMITER ;
