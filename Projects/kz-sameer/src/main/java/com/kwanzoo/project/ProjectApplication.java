61ae9e246cbbac95e0017ae86bfa5991ec7a50be
kz-sameer <sameer@kwanzoo.com>
2021-01-28 15:50:50 +0530

    configuration for redis

--- Projects/kz-sameer/src/main/java/com/kwanzoo/project/ProjectApplication.java
+++ Projects/kz-sameer/src/main/java/com/kwanzoo/project/ProjectApplication.java
@@ -2,12 +2,14 @@ package com.kwanzoo.project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@EnableCaching
 public class ProjectApplication {
-
+	
 	public static void main(String[] args) {
 		SpringApplication.run(ProjectApplication.class, args);
 	}
--- Projects/kz-sameer/src/main/java/com/kwanzoo/project/controller/Api.java
+++ Projects/kz-sameer/src/main/java/com/kwanzoo/project/controller/Api.java
@@ -1,13 +1,13 @@
 package com.kwanzoo.project.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.kwanzoo.project.model.Account;
+import com.kwanzoo.project.model.PageInfo;
+import com.kwanzoo.project.model.PagedReturn;
 import com.kwanzoo.project.service.ProjectService;
 
 @RestController
@@ -17,11 +17,11 @@ public class Api {
 	ProjectService service;
 	
 	@GetMapping("/accounts")
-	public Page<Account> getAccountsByPage( @ModelAttribute Account account,
-											@RequestParam(required = false, defaultValue = "0") int page,
-											@RequestParam(required = false, defaultValue = "10") int pageSize,
-											@RequestParam(required = false) String any){
-		return service.findBy(account, page, pageSize, any);	
+	public PagedReturn getAccountsByPage( @ModelAttribute Account account, @ModelAttribute PageInfo pageInfo){
+		
+		String cacheKey = account.toString() + pageInfo.toString();
+		
+		return service.findBy(account, pageInfo, cacheKey);	
 	}
 
 }
