5941227c46d0073fc7cb90dd5f6068049a0e11b4
= <=>
2021-01-28 22:47:27 +0530

    Change type to DateTime

--- Projects/kz-shiva/databaseScripts/activity/main.sql
+++ Projects/kz-shiva/databaseScripts/activity/main.sql
@@ -3,14 +3,14 @@ drop table if exists activity;
 create table activity (
     id varchar(100),
     buyer_id varchar(100),
-    datetime varchar(100),
+    date_time datetime,
     activity_type varchar(100),
     details varchar(100)
 );
 
 source activity/getDetails.sql;
 
-insert into activity(buyer_id, datetime, activity_type, details)
+insert into activity(buyer_id, date_time, activity_type, details)
 select opencx_buyer_id, from_unixtime(floor(date_and_time/1000)), activity_type, getDetails(activity_type, creative_name, website) from temp;
 
 -- set buyer_id as foreign key
