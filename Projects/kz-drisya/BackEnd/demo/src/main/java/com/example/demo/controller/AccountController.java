777e08fb4a24187f3c8717747afc86895aaf998e
Drisya POnmari <111701031@smail.iitpkd.ac.in>
2021-01-31 13:06:12 +0530

    Added endpoints buyer and activity

--- Projects/kz-drisya/BackEnd/demo/src/main/java/com/example/demo/controller/AccountController.java
+++ Projects/kz-drisya/BackEnd/demo/src/main/java/com/example/demo/controller/AccountController.java
@@ -4,7 +4,6 @@ import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Page;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -12,17 +11,26 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.model.Account;
+import com.example.demo.model.Activity;
 import com.example.demo.service.AccountService;
+import com.example.demo.service.ActivityService;
+import com.example.demo.service.BuyerService;
 import com.example.demo.service.MetricService;
 
 @RestController
 public class AccountController {
 
 	@Autowired
-	AccountService service;
+	AccountService accountService;
 	
 	@Autowired
 	MetricService metricService;
+	
+	@Autowired
+	BuyerService buyerService;
+	
+	@Autowired
+	ActivityService actService;
 	
 	@GetMapping("/")
 	public String home(){
@@ -34,13 +42,22 @@ public class AccountController {
 	@GetMapping("/accounts")
 	public List<Map<String,Object>> getAccounts( @ModelAttribute Account account, @RequestParam (required = false) Integer page, 
 			@RequestParam (required = false) Integer page_size, @RequestParam (required = false) String q, @RequestParam ( required = false) String metrics, @RequestParam ( required = false) String start, @RequestParam ( required = false) String end, @RequestParam ( required = false) String exclude) {
-		
-		Page <Account> accounts= service.getAllAccounts( account, page, page_size, q);
-		
-		
+		Page <Account> accounts= accountService.getAllAccounts( account, page, page_size, q);
 		return metricService.metricsFilter(accounts, metrics,start, end,exclude);
-		//return accounts;
 			
+	}
+	
+	@GetMapping("/buyer")
+	public List<Map<String,Object>> getBuyers(@RequestParam Map<String, String> Query, @RequestParam (required = false) Integer page, 
+			@RequestParam (required = false) Integer page_size){
+		
+		return buyerService.getBuyers(Query, page, page_size);
+	}
+	
+	@GetMapping("/activity")
+	public Page<Activity> getActivities(@RequestParam Map<String, String> Query, @RequestParam (required = false) Integer page, @RequestParam (required = false) Integer page_size){
+		
+		return actService.getActivity(Query, page, page_size);
 	}
 	
 }
